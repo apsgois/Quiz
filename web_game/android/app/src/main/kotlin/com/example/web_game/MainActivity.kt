f1a8812e710bec6ef8b071e0296bee222957c2c3
Ana Paula Serafim de Góis <68386929+apsgois@users.noreply.github.com>
2023-06-28 22:48:29 -0300

    Delete web_game directory

--- web_game/android/app/src/main/kotlin/com/example/web_game/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.web_game
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- web_game/lib/models/user_model.dart
+++
@@ -1,6 +0,0 @@
-class UserModel {
-  final String username;
-  final String password;
-
-  UserModel({required this.username, required this.password});
-}
